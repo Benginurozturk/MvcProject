55435b9ad0f1afdbe62c6f42442fb97a5df32b0d
Benginurozturk <77507752+Benginurozturk@users.noreply.github.com>
2021-05-19 20:12:00 +0300

    Murat Yücedağ Mvc Kampı ilk 10 ders

---
+++ MvcProject.DataAccessLayer/Context.cs
@@ -0,0 +1,15 @@
+﻿using MvcProject.EntityLayer.Entities;
+using System.Data.Entity;
+
+namespace MvcProject.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
